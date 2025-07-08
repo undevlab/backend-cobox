b5942d85d07f8f3a3fb5cf736f180578869228d9
RAUL ROBERTO SANCHEZ CRUZ <65100582+undevlab@users.noreply.github.com>
2025-07-08 16:17:36 -0500

    Update CorsConfig.java

--- src/main/java/com/cobox/fleet/config/CorsConfig.java
+++ src/main/java/com/cobox/fleet/config/CorsConfig.java
@@ -9,10 +9,14 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    registry.addMapping("/**")
+        .allowedOrigins(
+            "http://localhost:4200", 
+            "http://localhost:3000",
+            "https://cobox.ilabperu.pro"  // 👈 Agregar esta línea
+        )
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
     }
 }
